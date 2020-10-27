da6d076c6251f569e041604b47a51d51f165fff5
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-10-27 00:55:24 -0700

    Redesign

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -66,7 +66,7 @@ class App extends React.Component {
           tags.push('LIMITED<span>&nbsp;</span>EDITION');
         }
         if (data.isOnlineOnly) {
-          tags.push('ONLINE<span>&nbsp;</span>xONLY');
+          tags.push('ONLINE<span>&nbsp;</span>ONLY');
         }
         const item = {
           skuId: data.skuId,
@@ -130,6 +130,8 @@ class App extends React.Component {
           products: data,
         });
       });
+
+    document.location = '#step2';
   }
 
   handleInputChange(event) {
@@ -167,114 +169,132 @@ class App extends React.Component {
     }
     return (
       <div>
-        <h1>Sephora Email Grid Generator</h1>
-        <br />
-        <br />
-        <h2>Step 1: Generate your data</h2>
-        <form onSubmit={this.handleFormSubmit}>
-          <div>
-            <label htmlFor="links">
-              <br />
-              Paste your links here, one each line:
+        <header>
+          <div className="topline" />
+          <div className="logo">
+            <img src="/images/logo.svg" alt="Sephora logo" width="200" height="auto" />
+            {/* <img src="https://sephora.i.lithium.com/t5/image/serverpage/avatar-name/default-avatar/avatar-theme/sephora/avatar-collection/sephora/avatar-display-size/profile/version/2?xdesc=1.0" width="30" height="auto" alt="avatar" /> */}
+            <h1>Email Grid Generator</h1>
+          </div>
+          <div className="topline" />
+        </header>
+        <article className="fixed">
+          <h2>Step 1: Generate your data</h2>
+          <form onSubmit={this.handleFormSubmit}>
+            <div>
+              <label htmlFor="links">
+                <br />
+                Paste your links here, one each line:
+                {' '}
+                <br />
+                <br />
+                <textarea rows="25" cols="100" id="links" name="textareaValue" value={textareaValue} onChange={this.handleInputChange} />
+              </label>
+            </div>
+            <br />
+            <input type="submit" value="Submit" id="submit" />
+          </form>
+          <br />
+          <br />
+          <h2 id="step2">Step 2: Choose your preferences</h2>
+          <br />
+          <div id="typeofgrid">
+            <h3>Type of the grid</h3>
+            <label htmlFor="oneup">
+              1up grid
               {' '}
-              <br />
-              <br />
-              <textarea rows="20" cols="86" id="links" name="textareaValue" value={textareaValue} onChange={this.handleInputChange} />
+              <input type="radio" id="oneup" name="gridType" value="oneup" onChange={this.handleInputChange} checked={gridType === 'oneup'} />
+            </label>
+            <label htmlFor="twobyfour">
+              2x4 grid
+              {' '}
+              <input type="radio" id="twobyfour" name="gridType" value="twobyfour" onChange={this.handleInputChange} checked={gridType === 'twobyfour'} />
+            </label>
+            <label htmlFor="twobyfournew">
+              2x4
+              {' '}
+              <span className="newtag">NEW</span>
+              {' '}
+              grid
+              {' '}
+              <input type="radio" id="twobyfournew" name="gridType" value="twobyfournew" onChange={this.handleInputChange} checked={gridType === 'twobyfournew'} />
             </label>
           </div>
-          <br />
-          <input type="submit" value="Submit" id="submit" />
-        </form>
-        <br />
-        <br />
-        <h2>Step 2: Choose your preferences</h2>
-        <div id="typeofgrid">
-          <h3>Type of the grid:</h3>
-          <label htmlFor="oneup">
-            1up grid
-            {' '}
-            <input type="radio" id="oneup" name="gridType" value="oneup" onChange={this.handleInputChange} checked={gridType === 'oneup'} />
-          </label>
-          <label htmlFor="twobyfour">
-            2x4 grid
-            {' '}
-            <input type="radio" id="twobyfour" name="gridType" value="twobyfour" onChange={this.handleInputChange} checked={gridType === 'twobyfour'} />
-          </label>
-          <label htmlFor="twobyfournew">
-            2x4 NEW! grid
-            {' '}
-            <input type="radio" id="twobyfournew" name="gridType" value="twobyfournew" onChange={this.handleInputChange} checked={gridType === 'twobyfournew'} />
-          </label>
-        </div>
-        <div id="checkboxes">
-          <h3>Info:</h3>
-          <label htmlFor="showTags">
-            Show tags
-            {' '}
-            <input type="checkbox" id="showTags" name="showTags" checked={showTags} onChange={this.handleInputChange} />
-          </label>
-          <label htmlFor="showBrand">
-            Include Brand Name
-            {' '}
-            <input type="checkbox" id="showBrand" name="showBrand" checked={showBrand} onChange={this.handleInputChange} />
-          </label>
-        </div>
-        <div id="badges">
-          <h3>Badges:</h3>
-          <ul>
-            {products.map((product, index) => {
-              const name = `product${index}badge`;
-              return (
-                <React.Fragment key={product.skuId}>
-                  <li key={product.skuId}>{product.brandName}</li>
-                  <ul>
-                    {badges.map((badge, badgeIdx) => {
-                      const id = `product${index}badge${badgeIdx}`;
-                      return (
-                        <li key={product.skuId + badge.name}>
-                          <label htmlFor={id}>
-                            <input type="radio" id={id} name={name} checked={product.badge === badge} onChange={() => this.setBadge(index, badge)} />
-                            {' '}
-                            {badge.name}
-                          </label>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </React.Fragment>
-              );
-            })}
+          <div id="checkboxes">
+            <h3>Info</h3>
+            <label htmlFor="showTags">
+              Show tags
+              {' '}
+              <input type="checkbox" id="showTags" name="showTags" checked={showTags} onChange={this.handleInputChange} />
+            </label>
+            <label htmlFor="showBrand">
+              Include Brand Name
+              {' '}
+              <input type="checkbox" id="showBrand" name="showBrand" checked={showBrand} onChange={this.handleInputChange} />
+            </label>
+          </div>
+          <div id="badges">
+            <h3>Badges</h3>
+            <ul>
+              {products.map((product, index) => {
+                const name = `product${index}badge`;
+                return (
+                  <React.Fragment key={product.skuId}>
+                    <li key={product.skuId}>{product.brandName}</li>
+                    <ul>
+                      {badges.map((badge, badgeIdx) => {
+                        const id = `product${index}badge${badgeIdx}`;
+                        return (
+                          <li key={product.skuId + badge.name}>
+                            <label htmlFor={id}>
+                              <input type="radio" id={id} name={name} checked={product.badge === badge} onChange={() => this.setBadge(index, badge)} />
+                              {' '}
+                              {badge.name}
+                            </label>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </React.Fragment>
+                );
+              })}
 
-          </ul>
-        </div>
-
-        <div id="codewindow">
-          <h2>Step 3: Get your code</h2>
-          <div className="tab">
-            <button type="button" value="codeview" onClick={this.handleTabClick}>Generated Code</button>
-            <button type="button" value="preview" onClick={this.handleTabClick}>Preview</button>
+            </ul>
           </div>
           <br />
-          {activeTab === 'codeview'
-            && <textarea id="codeview" rows="20" cols="86" value={productsHtml} readOnly />}
-          {activeTab === 'preview'
-            // eslint-disable-next-line react/no-danger
-            && <div id="preview" dangerouslySetInnerHTML={{ __html: productsHtml }} />}
-        </div>
+          <h2>Step 3: Get your code</h2>
+          <br />
+          <div id="codewindow">
+            <div className="tab">
+              <button type="button" value="codeview" onClick={this.handleTabClick}>Generated Code</button>
+              <button type="button" value="preview" onClick={this.handleTabClick}>Preview</button>
+            </div>
+            <br />
+            {activeTab === 'codeview'
+              && <textarea id="codeview" rows="25" cols="100" value={productsHtml} readOnly />}
+            {activeTab === 'preview'
+              // eslint-disable-next-line react/no-danger
+              && <div id="preview" dangerouslySetInnerHTML={{ __html: productsHtml }} />}
+          </div>
 
-        <div id="examples">
-          <h3>Example links:</h3>
-          <textarea
-            id="links"
-            name="links"
-            rows="20"
-            cols="86"
-            defaultValue={`<a href="[@trackurl LinkID='' LinkName='dennisgrossdailypeel' LinkTag='pl-p4' LinkDesc='' Tracked='ON' Encode='OFF' LinkType='REDIRECT']https://www.sephora.com/product/P269122?skuId=1499482&$deep_link=true[/@trackurl]" target="_blank">
+          <div id="examples">
+            <h3>Example links</h3>
+            <textarea
+              id="links"
+              name="links"
+              rows="25"
+              cols="100"
+              defaultValue={`<a href="[@trackurl LinkID='' LinkName='shiseidofirmingset' LinkTag='pl-p1' LinkDesc='' Tracked='ON' Encode='OFF' OfferID='' LinkType='REDIRECT']https://www.sephora.com/product/P463953?skuId=2374296&$deep_link=true[/@trackurl]"  target="_blank">
+<a href="[@trackurl LinkID='' LinkName='saintjaneglowkit' LinkTag='pl-p2' LinkDesc='' Tracked='ON' Encode='OFF' OfferID='' LinkType='REDIRECT']https://www.sephora.com/product/P463935?skuId=2369841&$deep_link=true[/@trackurl]"  target="_blank">
+<a href="[@trackurl LinkID='' LinkName='slipbeautygiftset' LinkTag='pl-p3' LinkDesc='' Tracked='ON' Encode='OFF' OfferID='' LinkType='REDIRECT']https://www.sephora.com/product/P463350?skuId=2044139&$deep_link=true[/@trackurl]"  target="_blank">
+<a href="[@trackurl LinkID='' LinkName='hourglassconfessionsduo' LinkTag='pl-p4' LinkDesc='' Tracked='ON' Encode='OFF' OfferID='' LinkType='REDIRECT']https://www.sephora.com/product/P463862?skuId=2378610&$deep_link=true[/@trackurl]"  target="_blank">
+<a href="[@trackurl LinkID='' LinkName='dennisgrossdailypeel' LinkTag='pl-p4' LinkDesc='' Tracked='ON' Encode='OFF' LinkType='REDIRECT']https://www.sephora.com/product/P269122?skuId=1499482&$deep_link=true[/@trackurl]" target="_blank">
 <a href="[@trackurl LinkID='' LinkName='carolinaherreraparfum' LinkTag='pl-p5' LinkDesc='' Tracked='ON' Encode='OFF' LinkType='REDIRECT']https://www.sephora.com/product/P420533?skuId=1960707&$deep_link=true[/@trackurl]" target="_blank">
 <a href="[@trackurl LinkID='' LinkName='ctminilipsticklipliner' LinkTag='pl-p6' LinkDesc='' Tracked='ON' Encode='OFF' LinkType='REDIRECT']https://www.sephora.com/product/P458268?skuId=2339620&$deep_link=true[/@trackurl]" target="_blank">
 <a href="[@trackurl LinkID='' LinkName='pmgdivinerosepalette' LinkTag='pl-p7' LinkDesc='' Tracked='ON' Encode='OFF' LinkType='REDIRECT']https://www.sephora.com/product/P458276?skuId=2351542&$deep_link=true[/@trackurl]" target="_blank">`}
-          />
-        </div>
+            />
+          </div>
+        </article>
       </div>
     );
   }
