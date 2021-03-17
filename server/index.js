61a7fd25a8c96841e64a8ca2c59ab590062bb179
Rita Rozsavolgyi <rvrita.me@gmail.com>
2021-03-17 12:54:53 -0700

    Header fix

--- server/index.js
+++ server/index.js
@@ -9,14 +9,37 @@ const port = process.env.PORT || 8889;
 
 app.use(cors());
 
+// app.get('/skus/:sku', (req, res) => {
+//   const regionPath = (req.query.country === 'ca') ? 'ca/en/' : '';
+//   const url = `https://www.sephora.com/${regionPath}${path.join('api/catalog/skus', req.params.sku)}`;
+//   request(url, (error, response, body) => {
+//     if (!error && response.statusCode === 200) {
+//       res.setHeader('Set-Cookie', 'HttpOnly;Secure;SameSite=Strict');
+//       res.send(body);
+//     } else {
+//       res.status(500).send(error);
+//     }
+//   });
+// });
 app.get('/skus/:sku', (req, res) => {
   const regionPath = (req.query.country === 'ca') ? 'ca/en/' : '';
   const url = `https://www.sephora.com/${regionPath}${path.join('api/catalog/skus', req.params.sku)}`;
-  request(url, (error, response, body) => {
+  request({
+    url,
+    timeout: 5000,
+    agent: false,
+    pool: { maxSockets: 100 },
+    headers: {
+      // https://github.com/request/request/issues/2738#issuecomment-369324868
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': 'axios/0.18.0',
+    },
+  }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       res.setHeader('Set-Cookie', 'HttpOnly;Secure;SameSite=Strict');
       res.send(body);
     } else {
+      console.error(error);
       res.status(500).send(error);
     }
   });
